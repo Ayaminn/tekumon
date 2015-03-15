4f5e06c2a12403330b6875abe42097bcf970e92f
Ayami_Imai <ayami0727tu@yahoo.co.jp>
2015-03-15 18:49:52 +0900

    Initial Commit

---
+++ first_swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  first_swift
+//
+//  Created by Ayami_Imai on 2015/03/15.
+//  Copyright (c) 2015年 Ayami_Imai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
